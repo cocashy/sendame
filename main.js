edcc95c027bffd9453355127122f14e72cb506ac
shiofrui <tomo170119@gmail.com>
2022-06-05 20:13:27 +0900

    edit

--- main.js
+++ main.js
@@ -1,71 +1,108 @@
-let period = 2000;
-let phase = 0;
-let isOver = false;
-
-const commandDict = {
-  "防御": "X",
-  "溜め": "C",
-  "攻撃": "V"
+let game;
+window.onload = () => {
+  game = new Game();
 }
 
-const message = new class {
-  constructor() {
-    this.dom = document.getElementById("message");
+document.getElementById("commands").onclick = (e) => {
+  const input = e.target.textContent;
+  const commandDict = {
+    "防御": "X",
+    "溜め": "C",
+    "攻撃": "V"
   }
+  const key = commandDict[input];
+  if (key === undefined) return;
+  const keyDownEvent = new KeyboardEvent("keydown", { "key": key });
+  document.dispatchEvent(keyDownEvent);
 
-  send(message) {
-    this.dom.innerText = message;
+  if (!game.isStart) {
+    game.update();
+    game.isStart = true;
   }
 }
 
-const opponent = new class {
+document.onkeydown = (e) => {
+  const commandDict = {
+    "X": "防御",
+    "C": "溜め",
+    "V": "攻撃"
+  }
+  const key = e.key.toUpperCase();
+  entry.innerText = commandDict[key];
+}
+
+class Game {
   constructor() {
-    this.command = document.getElementById("opponent-command");
-    this.charge = document.getElementById("opponent-charge");
-    this.chargeCount = 0;
+    this.isStart = false;
+    this.isOver = false;
+    this.period = 2000;
+    this.phase = 0;
+
+    this.signal = new Signal("signal");
+    this.player = new Player();
+    this.opponent = new Opponent();
+    this.messageDOM = document.getElementById("message");
+    this.reloadButton = document.getElementById("reload-button");
+    this.reloadButton.onclick = location.reload.bind(location);
+    this.reloadButton.style.visibility = "hidden";
   }
 
-  chioceCommand() {
-    let options = Object.keys(commandDict);
-    if (player.chargeCount === 0) {
-      options = options.filter(option => option !== "防御");
+  update() {
+    if (this.phase === 0) {
+      this.signal.turnOn(0);
+
+    } else if (this.phase === 1) {
+      this.signal.turnOn(1);
+
+    } else if (this.phase === 2) {
+      this.signal.turnOn(2);
+
+      if (this.player.entryDOM.innerText === "") {
+        this.isOver = true;
+        this.messageDOM.innerText = "[ERROR] No command is entered...";
+      }
+
+      this.player.sendCommand();
+      this.opponent.sendCommand();
+
+      this.checkGameSet();
+
+    } else if (this.phase === 3) {
+      this.signal.turnOffAll();
+
+      this.player.updateCharge();
+      this.opponent.updateCharge();
+
+      this.player.commandDOM.innerText = "";
+      this.opponent.commandDOM.innerText = "";
     }
-    if (this.chargeCount === 0) {
-      options = options.filter(option => option !== "攻撃");
+
+    if (!this.isOver) {
+      this.phase = (this.phase + 1) % 4;
+      setTimeout(this.update.bind(this), this.period / 4);
+    } else {
+      this.reloadButton.style.visibility = "visible";
     }
-    if (this.chargeCount === 3) {
-      options = options.filter(option => option === "攻撃");
-    }
-    const chioced = options[Math.floor(Math.random() * options.length)];
-    this.command.innerText = chioced;
   }
 
-  clearCommand() {
-    this.command.innerText = "";
+  checkGameSet() {
+    if (this.player.commandDOM.innerText === "攻撃") {
+      if (this.opponent.commandDOM.innerText === "溜め" || this.player.charge.count >= 3) {
+        this.isOver = true;
+        this.messageDOM.innerText = "[GAME SET] You win!";
+      }
+    } else if (this.opponent.commandDOM.innerText === "攻撃") {
+      if (this.player.commandDOM.innerText === "溜め" || this.opponent.charge.count >= 3) {
+        this.isOver = true;
+        this.messageDOM.innerText = "[GAME SET] You lose...";
+      }
+    }
   }
 }
 
-const player = new class {
-  constructor() {
-    this.command = document.getElementById("player-command");
-    this.charge = document.getElementById("opponent-charge");
-    this.entry = document.getElementById("entry");
-    this.chargeCount = 0;
-  }
-
-  sendCommand() {
-    this.command.innerText = entry.innerText;
-    this.entry.innerText = "";
-  }
-
-  clearCommand() {
-    this.command.innerText = "";
-  }
-}
-
-const signal = new class {
-  constructor() {
-    this.dom = document.getElementById("signal");
+class Signal {
+  constructor(id) {
+    this.dom = document.getElementById(id);
     this.turnOffAll();
   }
 
@@ -73,58 +110,103 @@ const signal = new class {
     this.dom.children[index].style.visibility = "visible";
   }
 
+  turnOff(index) {
+    this.dom.children[index].style.visibility = "hidden";
+  }
+
   turnOffAll() {
     for (let i = 0; i < 3; i++) {
-      this.dom.children[i].style.visibility = "hidden";
+      this.turnOff(i);
     }
   }
 }
 
-document.getElementById("commands").onclick = (e) => {
-  const input = e.target.textContent;
-  const key = commandDict[input];
-  const keyDownEvent = new KeyboardEvent("keydown", { "key": key });
-  document.dispatchEvent(keyDownEvent);
-}
-
-document.onkeydown = (e) => {
-  entry.innerText = Object.keys(commandDict).filter(k => {
-    return commandDict[k] === e.key.toUpperCase();}
-  );
-}
-
-const startButton = document.getElementById("start-button");
-startButton.onclick = () => {
-  update();
-  startButton.style.visibility = "hidden";
-}
-
-const update = () => {
-  if (phase === 0) {
-    signal.turnOn(0);
-
-  } else if (phase === 1) {
-    signal.turnOn(1);
-
-  } else if (phase === 2) {
-    signal.turnOn(2);
-    if (entry.innerText === "") {
-      isOver = true;
-      message.send("[ERROR] No command is entered");
-    }
-
-    opponent.chioceCommand();
-    player.sendCommand();
-
-
-  } else if (phase === 3) {
-    signal.turnOffAll();
-    opponent.clearCommand();
-    player.clearCommand();
+class Charge extends Signal {
+  constructor(id) {
+    super(id);
   }
 
-  if (!isOver) {
-    phase = (phase + 1) % 4;
-    setTimeout(update, period / 4);
+  add() {
+    const count = this.count;
+    if (count >= 3) return;
+    this.turnOn(count);
+  }
+
+  sub() {
+    const count = this.count;
+    if (count <= 0) return;
+    this.turnOff(count-1);
+  }
+
+  get count() {
+    let count = 0;
+    for (let i = 0; i < 3; i++) {
+      if (this.dom.children[i].style.visibility === "visible") {
+        count++;
+      } else {
+        return count;
+      }
+    }
+    return count;
+  }
+}
+
+class Opponent {
+  constructor() {
+    this.commandDOM = document.getElementById("opponent-command");
+    this.charge = new Charge("opponent-charge");
+  }
+
+  updateCharge() {
+    if (this.commandDOM.innerText === "溜め") {
+      this.charge.add();
+    } else if (this.commandDOM.innerText === "攻撃") {
+      this.charge.sub();
+    }
+  }
+
+  sendCommand() {
+    let options = ["防御", "溜め", "攻撃"];
+    if (game.player.charge.count === 0) {
+      options = options.filter(option => option !== "防御");
+    }
+    if (this.charge.count === 0) {
+      options = options.filter(option => option !== "攻撃");
+    }
+    if (this.charge.count === 3) {
+      options = options.filter(option => option === "攻撃");
+    }
+    const chioced = options[Math.floor(Math.random() * options.length)];
+    this.commandDOM.innerText = chioced;
+  }
+}
+
+class Player {
+  constructor() {
+    this.attackButton = document.getElementById("attack-button");
+    this.commandDOM = document.getElementById("player-command");
+    this.entryDOM = document.getElementById("entry");
+    this.charge = new Charge("player-charge");
+    this.charge.turnOffAll();
+    this.attackButton.style.visibility = "hidden";
+  }
+
+  updateCharge() {
+    if (this.commandDOM.innerText === "溜め") {
+      this.charge.add();
+    } else if (this.commandDOM.innerText === "攻撃") {
+      this.charge.sub();
+    }
+
+    if (this.charge.count <= 0) {
+      this.attackButton.style.visibility = "hidden";
+    } else {
+      this.attackButton.style.visibility = "visible";
+    }
+  }
+
+  sendCommand() {
+    this.commandDOM.innerText = this.entryDOM.innerText;
+    this.entryDOM.innerText = "";
   }
 }
